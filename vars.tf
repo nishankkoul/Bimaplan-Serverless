d0fa3bd1726c5752f778c0e79566d5feda2a2f56
nishankkoul <koulnishank5@gmail.com>
2024-09-13 13:44:22 +0530

    Updated Terraform files

--- vars.tf
+++ vars.tf
@@ -7,8 +7,3 @@ variable "lambda_function_name" {
   description = "Name of the Lambda function"
   default     = "hello_world_function"
 }
-
-variable "function_version" {
-  description = "Version of the Lambda function"
-  default     = "1.0.0"
-}
